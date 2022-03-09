a11f7e9777b826debe4a9888e6572a55ed85a1ef
Xin Li <lixin39@eco-13.syslab.sandbox>
2022-03-09 17:39:35 -0500

    add measurements

---
+++ benchmarks/conv/measure.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CUDA_DEV=${1:-'2'}
+
+export CUDA_VISIBLE_DEVICES=$CUDA_DEV
+
+python conv_results_analysis.py 10000 --cost_model random --search_eps 0.05
+python conv_results_analysis.py 10000 --cost_model random --search_eps 0.5
+python conv_results_analysis.py 10000 --cost_model random --search_eps 1.0
+python conv_results_analysis.py 10000 --cost_model XGBoost --search_eps 0.05
+python conv_results_analysis.py 10000 --cost_model XGBoost --search_eps 0.5
+
