9804da2d945d8c6d951dcd7d4d319accac395514
Xin Li <lixin39@eco-13.syslab.sandbox>
2022-04-07 10:22:14 -0400

    add results

--- benchmarks/conv/run_exp.sh
+++ benchmarks/conv/run_exp.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 ITERS=${1:-'10'}
 
-CUDA_VISIBLE_DEVICES=0 python conv.py $ITERS --cost_model random  --search_eps 0.05  >& log0.txt &
-CUDA_VISIBLE_DEVICES=1 python conv.py $ITERS --cost_model random  --search_eps 0.5   >& log1.txt &
-CUDA_VISIBLE_DEVICES=2 python conv.py $ITERS --cost_model XGBoost --search_eps 0.05  >& log2.txt &
-CUDA_VISIBLE_DEVICES=3 python conv.py $ITERS --cost_model XGBoost --search_eps 0.5   >& log3.txt &
+CUDA_VISIBLE_DEVICES=3 python conv.py $ITERS --cost_model random  --search_eps 1.0   >& log5.txt 
+CUDA_VISIBLE_DEVICES=3 python conv.py $ITERS --cost_model random  --search_eps 0.05  >& log0.txt 
+CUDA_VISIBLE_DEVICES=3 python conv.py $ITERS --cost_model random  --search_eps 0.5   >& log1.txt 
+CUDA_VISIBLE_DEVICES=3 python conv.py $ITERS --cost_model XGBoost --search_eps 0.05  >& log2.txt 
+CUDA_VISIBLE_DEVICES=3 python conv.py $ITERS --cost_model XGBoost --search_eps 0.5   >& log3.txt 
 wait
 python conv_torch.py
 
